56f677c1b4d85aac521cce91df57135ccab9b734
jennachuu <srchoo19@gmail.com>
2024-10-02 12:47:49 +0900

    update comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // BrowserRouter import
-import App from './App'; // App import
-import './index.css'; // index.css import (필요에 따라)
+import { BrowserRouter } from 'react-router-dom'; 
+import App from './App'; 
+import './index.css'; 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
